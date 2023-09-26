f323b8d5d3147d9465a583cc46420451dbc10363
Enkratia <manofthetreasure@gmail.com>
2023-09-26 16:53:13 +0300

    m

---
+++ api/server.js
@@ -0,0 +1,26 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+// My code
+const jsonServerAuth = require("json-server-auth");
+server.use(jsonServerAuth);
+
+// Add this before server.use(router)
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  }),
+);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
