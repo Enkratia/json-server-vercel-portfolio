e0bc981688af43ac655f61518266164a07b20de2
Enkratia <manofthetreasure@gmail.com>
2023-09-26 23:25:46 +0300

    m

--- api/server.js
+++ api/server.js
@@ -27,10 +27,10 @@ server.db = router.db;
 server.use(middlewares);
 server.use(auth);
 
-// server.use(router);
-// server.listen(3000, () => {
-//   console.log("JSON Server is running");
-// });
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
 
 // Export the Server API
 module.exports = server;
