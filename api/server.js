6de40dc6037dd5a5d577bcf9d9e905c82af563c2
Enkratia <manofthetreasure@gmail.com>
2023-09-26 20:36:54 +0300

    m

--- api/server.js
+++ api/server.js
@@ -1,11 +1,9 @@
 // See https://github.com/typicode/json-server#module
-const path = require("path");
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 
 const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.db = router.db;
