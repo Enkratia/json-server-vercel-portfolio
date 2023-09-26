38ea6c6dfaed4b7457b713f889b0e02d0d2f490e
Enkratia <manofthetreasure@gmail.com>
2023-09-26 22:10:56 +0300

    m

--- api/server.js
+++ api/server.js
@@ -9,18 +9,18 @@ const middlewares = jsonServer.defaults();
 server.db = router.db;
 
 // Add this before server.use(router)
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  }),
-);
-server.use(
-  auth.rewriter({
-    users: 664,
-    messages: 664,
-    "/api/*": "/$1",
-  }),
-);
+// server.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//   }),
+// );
+// server.use(
+//   auth.rewriter({
+//     users: 664,
+//     messages: 664,
+//     "/api/*": "/$1",
+//   }),
+// );
 
 // My code
 server.use(middlewares);
