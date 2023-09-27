5dad74368b8c414abf9e64c6b0e9a4b4d0a93439
Enkratia <manofthetreasure@gmail.com>
2023-09-27 19:30:06 +0300

    m

--- api/server.js
+++ api/server.js
@@ -18,16 +18,5 @@ server.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
 
-exports.handler = async (event, context) => {
-  const response = {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify({ message: "Hello World" }),
-  };
-  return response;
-};
-
 // Export the Server API
 module.exports = server;
