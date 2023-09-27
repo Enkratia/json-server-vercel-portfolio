fad65f445ef1154b1059536eb9e82b5c4d2ed6e5
Enkratia <manofthetreasure@gmail.com>
2023-09-27 19:36:44 +0300

    m

--- api/server.js
+++ api/server.js
@@ -20,3 +20,17 @@ server.listen(PORT, () => {
 
 // Export the Server API
 module.exports = server;
+
+module.exports = function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "YOUR_URL"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // Set custom headers for CORS
+  res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Custom-Header");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  return next();
+};
