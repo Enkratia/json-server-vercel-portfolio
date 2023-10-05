9306dd52e59d93965c063dc6b656db21401964c7
Enkratia <manofthetreasure@gmail.com>
2023-10-05 15:21:12 +0300

    m

--- api/server.js
+++ api/server.js
@@ -3,14 +3,32 @@ const auth = require("json-server-auth");
 
 const server = jsonServer.create();
 
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
 
-server.db = router.db;
+// server.db = router.db;
 
 // Add this before server.use(router)
+// server.use(middlewares);
+// server.use(auth);
+
+//****************** */
+// Uncomment to allow write operations
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
+
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults();
+
 server.use(middlewares);
 server.use(auth);
+//************ */
 
 server.use(router);
 server.listen(3000, () => {
