1def5a5b83150858b911936068f605eecc05fd2c
Enkratia <manofthetreasure@gmail.com>
2023-10-05 15:45:28 +0300

    m

--- api/server.js
+++ api/server.js
@@ -1,22 +1,12 @@
+const fs = require("fs");
+const path = require("path");
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 
 const server = jsonServer.create();
-
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
-
-// server.db = router.db;
+const middlewares = jsonServer.defaults();
 
 // Add this before server.use(router)
-// server.use(middlewares);
-// server.use(auth);
-
-//****************** */
-// Uncomment to allow write operations
-const fs = require("fs");
-const path = require("path");
-
 const filePath = path.join("db.json");
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
@@ -24,15 +14,10 @@ const db = JSON.parse(data);
 const router = jsonServer.router(db);
 server.db = router.db;
 
-// Comment out to allow write operations
-// const router = jsonServer.router('db.json')
-
-const middlewares = jsonServer.defaults();
-
 server.use(middlewares);
 server.use(auth);
-//************ */
 
+// Add server.use(router)
 server.use(router);
 server.listen(3000, () => {
   console.log("Server started");
